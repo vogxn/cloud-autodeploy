2af52f383eb7b25bd655a4dd0efbf9b38a8f3d8b
Prasanna Santhanam <Prasanna.Santhanam@citrix.com>
2012-10-22 20:41:04 +0530

    just incase open with marvin doesn't work

--- configure.py
+++ configure.py
@@ -341,8 +341,11 @@ if __name__ == '__main__':
         _openIntegrationPort(cscfg)
         mgmt_ip = mactable[mgmt_host]["address"]
         mgmt_pass = mactable[mgmt_host]["password"]
-        # Open up 8096 for Marvin initial signup and register
         ssh = remoteSSHClient.remoteSSHClient(mgmt_ip, 22, "root", mgmt_pass) 
+
+        ssh.execute("mysql -ucloud -Dcloud -pcloud -e'update configuration set value=%s where name=%s'" %(csconfig.mgtSvr[0].port, 'integration.api.port') )
+
+        # Open up 8096 for Marvin initial signup and register
         ssh.execute("service cloud-management restart")
     else:
         raise Exception("Reqd services (ssh, mysql) on management server are not up. Aborting")
